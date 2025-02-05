c179dd4a249d76dd84cc6a6d33c84f531d8b0a9b
dimasik <jysipysi31@gmail.com>
2025-02-05 18:12:31 +0200

    Initial commit

---
+++ public/script.js
@@ -0,0 +1,7 @@
+// Выбираем кнопку по классу
+const button = document.querySelector('.button');
+
+// Добавляем обработчик события на кнопку
+button.addEventListener('click', function() {
+    alert('Кнопка нажата!');
+});
